dd21a456f77919f8b9f82548df850f1c7b49f7a8
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2018-06-14 00:28:53 -0400

    added component pages

--- clicknfl/src/App.js
+++ clicknfl/src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Memory Challenge Game</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+         To win the game, you must click on each icon ONCE. If you click the same icon more than once you lose!
         </p>
       </div>
     );
