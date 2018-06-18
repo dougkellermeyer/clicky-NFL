27f107e24a0c764beb1336b83da1c064c25e3f94
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2018-06-17 22:17:16 -0400

    updated nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,7 @@ import React from 'react';
 const Header = (props)=> (
     <nav style={{background: "purple", color: "white"}} className={"navbar navbar-default sticky-top"}>
         <div className={"conatiner"}>
-            <div>
-                <h1>Twitch Clicker</h1>
-                </div>
+            <div>Twitch Clicker</div>
             <div>score:{props.score}</div>
         </div>
     </nav>
